a40a56d4284a5e2e35bbf7ec29b0993a99768633
Gavin Coombes <gjcoombes@gmail.com>
2013-11-12 19:59:24 +0800

    py -> cy

--- pythresh/pythresh.py
+++ pythresh/pythresh.py
@@ -27,7 +27,7 @@ from shapely.geometry import Polygon
 #
 #import bonaparte
 #import bonaparte.stoch.stoch_lib as sl
-#from bonaparte.utils.grid_cell_areas import grid_cell_areas
+#import bonaparte.utils.grid_cell_areas as ga
 
 ### Logging
 import logging
@@ -50,18 +50,18 @@ EMPTY_HEADER_LINES = 7
 ### Functions
 
 ### Grid cell area module
-def grid_cell_areas(_grid_header=None, grid_fp=None):
+def ga_grid_cell_areas(grid_header=None, grid_fp=None):
     """
     Return an array of the areas of a column of grid cells
     """
-    assert bool(_grid_header) != osp.isfile(grid_fp or "")
-    _gh = _grid_header or grid_header(grid_fp)
-    points_array = grid_column_verts(_gh)
-    utm_points_array = reproject(points_array)
-    area_array = polygon_areas(utm_points_array)
+    assert bool(grid_header) != osp.isfile(grid_fp or "")
+    _gh = grid_header or sl_grid_header(grid_fp)
+    points_array = ga_grid_column_verts(_gh)
+    utm_points_array = ga_reproject(points_array)
+    area_array = ga_polygon_areas(utm_points_array)
     return area_array
 
-def grid_column_verts(_grid_header=None, grid_fp=None):
+def ga_grid_column_verts(grid_header=None, grid_fp=None):
     """
     Given a grid, return an array of the corners of a column of the grid
     Which column is unimportant as all will have the same set of areas.
@@ -79,15 +79,15 @@ def grid_column_verts(_grid_header=None, grid_fp=None):
      http://stackoverflow.com/questions/5891410/numpy-array-initialization-fill-with-identical-values?lq=1
     """
     # Ensure  grid_header xor grid_fp exist
-    assert bool(_grid_header) != osp.isfile(grid_fp or "")
-    _gh = _grid_header or grid_header(grid_fp)
+    assert bool(grid_header) != osp.isfile(grid_fp or "")
+    _gh = grid_header or sl_grid_header(grid_fp)
     n_rows = _gh['n_rows']
     dy     = float(_gh['lon_delta'])
     lon_0  = float(_gh['lon_lower_left'])
     lat_0  = float(_gh['lat_lower_left'])
 
     # Make me a function to generate vertices
-    vertices = verts_factory(_gh)
+    vertices = ga_verts_factory(_gh)
     # Need a sequence of lower left points
     verts_array = np.empty(shape=(n_rows, 5, 2))
     ll_corners = np.empty(shape=(n_rows, 2)) # lower left corners (lon, lat)
@@ -96,7 +96,7 @@ def grid_column_verts(_grid_header=None, grid_fp=None):
     verts_array[:] = np.array(map(vertices, ll_corners))
     return verts_array
 
-def polygon_areas(arr):
+def ga_polygon_areas(arr):
     """
     Given an column of points, return a column of polygon areas
     """
@@ -104,17 +104,17 @@ def polygon_areas(arr):
     areas = [p.area for p in ps]
     return np.array(areas)
 
-def reproject(arr, zone=None):
+def ga_reproject(arr, zone=None):
     """Given an aray of points, return the utm coordinates"""
     new_arr = np.empty_like(arr)
-    _zone = zone or utm_zone(None)
+    _zone = zone or ga_utm_zone(None)
     proj = Proj(proj="utm", zone=_zone, ellps="WGS84")
     for i, grid_cell in enumerate(arr):
         for j, point in enumerate(grid_cell):
             new_arr[i, j, :] = np.array(proj(*point))
     return new_arr
 
-def utm_zone(point):
+def ga_utm_zone(point):
     """
     *** Warning stub only - fixed output
     Given a geographical point, return the appropriate utm zone
@@ -129,7 +129,7 @@ def utm_zone(point):
     warn("***Warning stub function - fixed return value***")
     return "50L"
 
-def verts_factory(grid_header):
+def ga_verts_factory(grid_header):
     """
     Return a function that will calculate the five verts given the lower left corner
     """
@@ -151,7 +151,7 @@ def verts_factory(grid_header):
 ### Stoch_lib module ###
 
 
-def gridder_arr_factory(grid_fp=None,
+def sl_gridder_arr_factory(grid_fp=None,
                         grid_ext=None,
                         grid_spc=None):
     """Return a function to convert lon, lat to row, col
@@ -163,15 +163,15 @@ def gridder_arr_factory(grid_fp=None,
         grid_spacing_arr - array of lon_delta and lat_delta
             see stoch.lib.grid_spacing
     """
-    err_msg = "Incomplete args. see \n {}".format(gridder_arr_factory.__doc__)
-    assert grid_fp or (grid_extent and grid_spacing), err_msg
+    err_msg = "Incomplete args. see \n {}".format(sl_gridder_arr_factory.__doc__)
+    assert grid_fp or (grid_ext and grid_spc), err_msg
 
     if grid_fp is not None:
         assert osp.isfile(grid_fp), "{} is not a file".format(grid_fp)
         if grid_ext is None:
-            grid_ext = grid_extent(grid_fp)
+            grid_ext = sl_grid_extent(grid_fp)
         if grid_spc is None:
-            grid_spc = grid_spacing_arr(grid_fp)
+            grid_spc = sl_grid_spacing_arr(grid_fp)
 
     origin = np.array(grid_ext['upper_left'])
     print("origin is {}".format(origin))
@@ -187,12 +187,12 @@ def gridder_arr_factory(grid_fp=None,
         return res_arr.astype(np.uint16)
     return _inner
 
-def grid_extent(fp):
+def sl_grid_extent(fp):
     """Return the upper left and lower right corners as lon, lat pairs
 
     Maybe return a dictionary instead?
     """
-    h = grid_header(fp)
+    h = sl_grid_header(fp)
     debug("grid header is")
     debug(h)
     # The upper left corner is closest to the 0, 0 the geographical origin
@@ -203,7 +203,7 @@ def grid_extent(fp):
     return {'upper_left' : [float(x) for x in [upper_left_lon,  upper_left_lat ]],
             'lower_right': [float(x) for x in [lower_right_lon, lower_right_lat]]}
 
-def grid_header(fp):
+def sl_grid_header(fp):
     """Grab the header from the grid file, return as numpy record array"""
     dt_names = ['h1', 'h2', 'lon_lower_left', 'lat_lower_left',
                 'lon_delta', 'lat_delta', 'n_cols', 'n_rows']
@@ -213,7 +213,7 @@ def grid_header(fp):
         header = np.fromfile(fh, dtype=dt, count=1)
     return header
 
-def grid_mass_dense(particles, gridder, grid_shape):
+def sl_grid_mass_dense(particles, gridder, grid_shape):
     """Grid the mass of the particles and return a dense array
 
     Args:
@@ -228,7 +228,7 @@ def grid_mass_dense(particles, gridder, grid_shape):
     mass_coo = sp.coo_matrix((_data, (_row, _col)), shape=grid_shape)
     return mass_coo.todense()
 
-def grid_mass_csr(particles, gridder, grid_shape):
+def sl_grid_mass_csr(particles, gridder, grid_shape):
     """Grid the mass of the particles and return a dense array
 
     Args:
@@ -243,20 +243,21 @@ def grid_mass_csr(particles, gridder, grid_shape):
     mass_coo = sp.coo_matrix((_data, (_row, _col)), shape=grid_shape)
     return mass_coo.tocsr()
 
-def grid_spacing_arr(fp):
+def sl_grid_spacing_arr(fp):
     """Return the grid spacing as dictionary """
-    h = grid_header(fp)
+    h = sl_grid_header(fp)
     res_arr = h[['lon_delta', 'lat_delta']].copy()
     # Change sign of lat_delta for decreasing (-ve) lat from origin
     res_arr.dtype.names = ['lon', 'lat']
     # TODO - Make it work for the northern hemisphere
+    # Should be easiest to adopt a lowerleft orgin for both hemispheres
     if SOUTHERN_HEMISPHERE:
         res_arr['lat'] = -np.abs(res_arr['lat'])
 #    res_arr['lat'] = res_arr['lat'] * -1
     return  res_arr
 
 
-def lu3_data(lu3_fp):
+def sl_lu3_data(lu3_fp):
     """Return binary data from file"""
     dtype_ls = [('iver', '<i4'), ('SIMTIME', '<i4'),
                 ('time', '<f4'), ('rec1st', '<i4'), ('rec2st', '<i4'),
@@ -269,7 +270,7 @@ def lu3_data(lu3_fp):
                 ('rat1end', '<i4')]
     return np.fromfile(lu3_fp, np.dtype(dtype_ls))
 
-def particles_and_shore_generator(tr3_fp, lu3_fp, grid_fp):
+def sl_particles_and_shore_generator(tr3_fp, lu3_fp, grid_fp):
     """Return a generator of particles and shore cells
     Note mismatch in dtype lengths between particles and shore
     Usage:
@@ -286,11 +287,11 @@ def particles_and_shore_generator(tr3_fp, lu3_fp, grid_fp):
         log("Grid file is {}".format(grid_fp))
 
 
-    grid_record = grid_extent(grid_fp)
+    grid_record = sl_grid_extent(grid_fp)
     lower_lon, upper_lat = grid_record['upper_left']
     upper_lon, lower_lat = grid_record['lower_right']
 
-    lu3_arr = lu3_data(lu3_fp)
+    lu3_arr = sl_lu3_data(lu3_fp)
     particle_names_ls = ['lon', 'lat', 'radius', 'prev_lon', 'prev_lat',
                          'type', 'mass', 'density', 'viscosity', 'age']
     particle_formats_ls = ['<f4'] * 5 + ['<i4'] + ['<f4'] * 4
@@ -339,22 +340,22 @@ def main():
 
     surf_threshold = 1e-6 # T/m2 or 1 g/m2
 
-    header = grid_header(grid_fp)
+    header = sl_grid_header(grid_fp)
     grid_shape = (header['n_rows'], header['n_cols'])
 
-    particles = particles_and_shore_generator(tr3_fp, lu3_fp, grid_fp)
-    gridder = gridder_arr_factory(grid_fp)
+    particles = sl_particles_and_shore_generator(tr3_fp, lu3_fp, grid_fp)
+    gridder = sl_gridder_arr_factory(grid_fp)
 
     start_time = time.time()
     max_surf_mass = np.zeros(grid_shape, dtype=np.float32)
 
     for i, tup in enumerate(particles):
         sim_time, surf, entr, arom, shore = tup
-        surf_dense = grid_mass_dense(surf, gridder, grid_shape)
+        surf_dense = sl_grid_mass_dense(surf, gridder, grid_shape)
         max_surf_mass = np.maximum(max_surf_mass, surf_dense)
 
     # Now we need a threshold_matrix to find which cells have exceeded the threshold
-    cell_areas = grid_cell_areas(grid_fp=grid_fp)
+    cell_areas = ga_grid_cell_areas(grid_fp=grid_fp)
     # the mass threshold is the threshold * area eg 0.001 kg/m2 * 640000 m2 = mass in Ts
     mass_threshold_T = cell_areas * surf_threshold
     exceedance = max_surf_mass >= mass_threshold_T[:, np.newaxis]
