8f54c63e30862700cecb437c98f8d357cef5da02
Gavin Coombes <gjcoombes@gmail.com>
2014-01-27 17:24:27 +0800

    start banks grids module

---
+++ grids/grids.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Module: grids.py
+Created on Mon Jan 27 17:21:40 2014
+@author: gav
+Description:
+Main module for manipulation and parsing of ASA dep and hab grids
+"""
+### Imports
+from __future__ import print_function
+
+### Logging
+import logging
+logging.basicConfig(level=logging.DEBUG)
+debug, info, error = logging.debug, logging.info, logging.error
+### Constants
+
+### Classes
+
+### Functions
+
+### Tests
+
+if __name__ == "__main__":
+
+    print("Done __main__")
