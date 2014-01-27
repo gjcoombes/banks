8bffd5fc0d7c5005c6541974b27b36d9e8aefc4c
Gavin Coombes <gjcoombes@gmail.com>
2014-01-27 17:18:36 +0800

    new central module tie.py

---
+++ gis/tie.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Module: tie.py
+Created on Mon Jan 27 17:12:19 2014
+@author: gav
+Description:
+Utilities for converting to and from ASA's tie polygon format
+
+The central common format will be geojson records suitable for the
+shapely/fiona gis stack.
+"""
+### Imports
+from __future__ import print_function
+
+### Logging
+import logging
+logging.basicConfig(level=logging.DEBUG)
+debug, info, error = logging.debug, logging.info, logging.error
+### Constants
+
+### Classes
+
+### Functions
+
+### Tests
+
+if __name__ == "__main__":
+
+    print("Done __main__")
